10fd7c4dd02f4e12164a20f3a193c86f0ad12133
Prashanth Ellina <prashanthellina@gmail.com>
2016-08-09 08:48:25 -0700

    fixes #7, added support

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="basescript",
-    version='0.1',
+    version='0.1.1',
     description="Basic infrastructure for writing scripts",
     keywords='basescript',
     author='Prashanth Ellina',
