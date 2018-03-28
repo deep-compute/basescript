7e847b5eff9d76b935630a864e10a2eb4357734c
root <root@node6.rq.st>
2018-03-28 11:39:44 +0200

    #61 exposed init_logger and get_logger

--- basescript/__init__.py
+++ basescript/__init__.py
@@ -1,3 +1,4 @@
 from __future__ import absolute_import
 
 from .basescript import BaseScript
+from .log import init_logger, get_logger
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def get_long_description():
 
 long_description = get_long_description()
 
-version = '0.2.3'
+version = '0.2.4'
 setup(
     name="basescript",
     version=version,
